e91c9fa52b26574615e4b9967431ae2f0edd755f
Lucian <odgon@users.noreply.github.com>
2021-12-01 00:21:39 +0100

    shadowing

--- concepts/vars_mut/src/main.rs
+++ concepts/vars_mut/src/main.rs
@@ -1,6 +1,18 @@
 fn main() {
+    // mutable
     let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    // shadowing
+    let y = 5;
+    let y = y + 1;
+
+    {
+        let y = y * 2;
+        println!("The value of y in the inner scope is: {}", y);
+    }
+
+    println!("The value of y is: {}", y);
 }
